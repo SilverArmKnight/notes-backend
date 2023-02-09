d691df3bc2d37881c99aff304fbe723dacbd79da
Your Name <you@example.com>
2023-02-09 16:35:27 +0700

    Push baby push

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const Note = require('./models/note')
+//const cors = require('cors')
+
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -42,7 +47,9 @@ app.get('/', (request, response) => {
 })
   
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -91,7 +98,7 @@ app.delete('/api/notes/:id', (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
